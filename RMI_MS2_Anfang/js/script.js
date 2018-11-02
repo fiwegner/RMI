a27fae3d94730274d60aeb19f65e6df974b15d0b
fiwegner <44700112+fiwegner@users.noreply.github.com>
2018-11-02 13:20:16 +0100

    Add files via upload

---
+++ RMI_MS2_Anfang/js/script.js
@@ -0,0 +1,22 @@
+
+
+
+/*var number = 1;
+$(document).ready(
+	function()
+	{
+		//callbackAnimation();
+		$('#wrapper ul.list > li').each(
+			function()
+			{
+				$(this).html(number+'.').css('padding-left', number*10);
+				number++;
+			}
+		);
+	}
+);
+
+function callbackAnimation()
+{
+	$('#wrapper > h2').html('Dies ist Inhalt').fadeOut(1000).fadeIn(2000).animate({'margin-top': 500}, 1000);
+}*/
